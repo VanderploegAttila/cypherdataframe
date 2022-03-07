79f131165a3b987fd1a0be5477ddae394474ada4
A-Ploeg <attila.vanderploeg@garnercorp.com>
2022-03-06 21:21:21 -0500

    Refactor cypher fragment

--- cydflib/cypher.py
+++ cydflib/cypher.py
@@ -1,10 +1,10 @@
+import pandas as pd
+
 from cydflib.model.Config import Config
 from cydflib.model.Query import Query
 from py2neo import Node, Relationship, Graph, Path, Subgraph
-from py2neo import NodeMatcher, RelationshipMatcher
 
-
-def query_to_dataframe(query: Query, config: Config):
+def query_to_dataframe(query: Query, config: Config) -> pd.DataFrame:
     cypher = query.cypher_query()
     graph = Graph(
         config.neo4j_url,
--- cydflib/model/Query.py
+++ cydflib/model/Query.py
@@ -1,23 +1,36 @@
 from dataclasses import dataclass
+import functools
+import operator
+from cydflib.model.Branch import Branch
+from cydflib.model.Node import Node
 
 
 @dataclass(frozen=True)
 class Query:
-    core_node_label: str
-    core_node_properties: list[str]
+    core_node: Node
+    branches: list[Branch]
+    skip: int = 0
+    limit: int = 10000
 
+    def cypher_query(self) -> str:
+        matches = [
+                      f'match(corenode:{self.core_node.label}) ' +
+                      f'with corenode skip {self.skip} limit {self.limit}'
+                  ] + [
+                      branch.cypher_fragment() for branch in self.branches
+                  ]
 
-    def cypher_query(self):
-        corenode_properties_string = \
-            ",".join(
-                [
-                    f" corenode.{node_property} "
-                    for node_property in self.core_node_properties
-                ]
-            )
-        result_string = \
-            f'''
-            match(corenode:{self.core_node_label}) 
-            return {corenode_properties_string} limit 10000;
-            '''
-        return result_string
+        all_nodes = [f"corenode.{prop.label}" for prop in self.core_node.properties]
+        all_nodes += functools.reduce(
+            operator.iconcat,
+            [
+                branch.branch_node.return_properties()
+                for branch in self.branches
+            ],
+            []
+        )
+
+        return_fragment = "return " + ",".join(all_nodes) + ';'
+        fragments = matches + [return_fragment]
+
+        return " ".join(fragments)
